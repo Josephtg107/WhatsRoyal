9090eec9d9619b053874b50ec6011122e1221edf
Jose Teodoro Garcia <josephtg107@gmail.com>
2024-05-30 09:21:08 -0500

    Initial Commit

---
+++ WhatsRoyal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsRoyal
+//
+//  Created by Joseph Garcia on 30/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsRoyal/WhatsRoyalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsRoyalApp.swift
+//  WhatsRoyal
+//
+//  Created by Joseph Garcia on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsRoyalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
